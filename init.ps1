7517c0af9ab4ac4a8f55ae4b909736bd894f2701
Tommaso Chiti <chitilivorno@gmail.com>
2021-11-25 17:11:52 +0100

    Create init.ps1

---
+++ init.ps1
@@ -0,0 +1,14 @@
+$url = "https://github.com/classy-giraffe/WinShush/archive/main.zip"
+$output = "WinShush.zip"
+$folder = "WinShush"
+if (Test-Path -Path $output -PathType Leaf) {
+    Write-Host "Archive already existent, deleting it."
+    Remove-Item -Path $output -Force
+}
+Invoke-WebRequest -Uri $url -OutFile $output
+if (Test-Path -Path $folder) {
+    Write-Host "Folder already existent, deleting it."
+    Remove-Item -Path $folder -Force -Recurse
+}
+Expand-Archive -Path $output -DestinationPath $folder
+Invoke-Item "WinShush\WinShush-main\winactivate\winactivate.cmd"
