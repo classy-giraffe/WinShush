9dcd4e711e55e3cb370739f308436c281b8e8542
Tommaso Chiti <chitilivorno@gmail.com>
2021-12-06 19:28:24 +0100

    Cleaning up code.

--- init.ps1
+++ init.ps1
@@ -1,16 +1,14 @@
 $url = "https://github.com/classy-giraffe/WinShush/archive/main.zip"
 $download_folder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-$output = $download_folder + "\WinShush.zip"
-if (Test-Path -Path $output -PathType Leaf) {
-    Write-Host "Archive already existent, deleting it."
-    Remove-Item -Path $output -Force
-}
-Invoke-WebRequest -Uri $url -OutFile $output
-if (Test-Path -Path $folder) {
-    Write-Host "Folder already existent, deleting it."
-    Remove-Item -Path $folder -Force -Recurse
-}
-Expand-Archive -Path $output -DestinationPath $download_folder
+$winshush_zip = $download_folder + "\WinShush.zip"
+$winshush_folder = $download_folder + "\WinShush"
+
+Invoke-WebRequest -Uri $url -OutFile $winshush_zip
+Expand-Archive -Path $winshush_zip -DestinationPath $winshush_folder
 Invoke-Item "WinShush\WinShush-main\winactivate\winactivate.cmd"
+
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
+
+Remove-Item -Path $winshsuh_zip -Force
+Remove-Item -Path $winshush_folder -Force -Recurse
