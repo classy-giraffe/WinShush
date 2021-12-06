776561d137ce1ed00ea9ff88a38e7efe4f4345ce
Tommaso Chiti <chitilivorno@gmail.com>
2021-12-06 19:13:13 +0100

    Fixing download issue.

--- init.ps1
+++ init.ps1
@@ -1,6 +1,6 @@
 $url = "https://github.com/classy-giraffe/WinShush/archive/main.zip"
-$output = "WinShush.zip"
-$folder = "WinShush"
+$download_folder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+$output = $download_folder + "\WinShush.zip"
 if (Test-Path -Path $output -PathType Leaf) {
     Write-Host "Archive already existent, deleting it."
     Remove-Item -Path $output -Force
@@ -10,7 +10,7 @@ if (Test-Path -Path $folder) {
     Write-Host "Folder already existent, deleting it."
     Remove-Item -Path $folder -Force -Recurse
 }
-Expand-Archive -Path $output -DestinationPath $folder
+Expand-Archive -Path $output -DestinationPath $download_folder
 Invoke-Item "WinShush\WinShush-main\winactivate\winactivate.cmd"
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
